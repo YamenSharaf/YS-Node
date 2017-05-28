bacad90101340330d48cba73ff667b4ff9f7024d
Yamen Sharaf <YamenSharaf@Gmail.com>
2017-05-28 04:26:01 +0200

    modularized

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
+//Setting up IO and Express
 const fs = require('fs');
 const express = require ('express'); const app = express();
-var pd = JSON.parse(fs.readFileSync('./data/portfolio.json'), undefined, 2);
-var posts = JSON.parse(fs.readFileSync('./data/blog-posts.json'), undefined, 2);
 
+//Importing JSON data
+var portfolio = JSON.parse(fs.readFileSync('./data/portfolio-items.json'), undefined, 2);
+var blog = JSON.parse(fs.readFileSync('./data/blog-posts.json'), undefined, 2);
+
+//Setting view engine and static files
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+
+//Setting environment variables
 const port = process.env.PORT || 3000;
 
+//Starting server
 app.listen (port, (success)=> {
   console.log(`Server started on port ${port}`);
 });
 
-
+//Rendering index template and passing JSON data
 app.get('/', (req, res)=>{
   res.render ('index', {
-    portfolio: pd.posts,
-    blog: posts.posts
+    portfolio: portfolio.posts,
+    blog: blog.posts
   });
 });
